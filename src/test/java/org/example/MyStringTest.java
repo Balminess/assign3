ef3f6c755a7598505d6052ad19112c45303d39a3
Balminess <hpf30248@gmail.com>
2024-03-19 16:35:04 -0500

    blank method

---
+++ src/test/java/org/example/MyStringTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class MyStringTest extends TestCase {
+
+    public void testIndexOfString() {
+        int result=MyString.indexOfString("hello world","world",0);
+        assertEquals(6,result);
+    }
+}
